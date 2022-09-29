80f536a9c18336905e8642459f9fd718ac424767
Jowan Jomon <jowanjomon619@gmail.com>
2022-09-29 10:39:27 +0400

    addded js

---
+++ main.js
@@ -0,0 +1,5 @@
+const button = document.querySelector(".giftImage1");
+button.addEventListener("click", () => {
+  const element = document.querySelector(".giftImage1");
+  element.style.backgroundimage = "./assets/salma.jpg";
+});
