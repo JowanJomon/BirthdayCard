3491e2b86668636c3d0027f7b156092041af602a
Jowan Jomon <jowanjomon619@gmail.com>
2022-09-29 11:36:13 +0400

    addded js to the whole site

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
-function hello() {
+//Function that makes the site work on mobile devices its basic js and the code needs to be minimalised as  of  now the code is repeating
+function ImageDisplay1() {
   document.getElementById("giftImage1").style.backgroundImage =
     "url(./assets/salma.jpg)";
 
   document.getElementById("giftImage1").style.backgroundSize = "cover";
 
   document.getElementById("giftImage1").style.backgroundPosition = "center";
+}
+
+function ImageDisplay2() {
+  document.getElementById("giftImage2").style.backgroundImage =
+    "url(./assets/smart.jfif)";
+
+  document.getElementById("giftImage2").style.backgroundSize = "cover";
+
+  document.getElementById("giftImage2").style.backgroundPosition = "center";
+}
+
+function ImageDisplay3() {
+  document.getElementById("giftImage3").style.backgroundImage =
+    "url(./assets/amelia.jfif)";
+
+  document.getElementById("giftImage3").style.backgroundSize = "cover";
+
+  document.getElementById("giftImage3").style.backgroundPosition = "center";
 }
