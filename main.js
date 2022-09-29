44dfaf7096d8b47e379150322eb7fb0a92593089
Jowan Jomon <jowanjomon619@gmail.com>
2022-09-29 11:24:38 +0400

    addded js and test 2

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 function hello() {
-  if (window.outerWidth && window.outerWidth > 480) {
-    document.getElementById("giftImage1").style.backgroundImage =
-      "url(./assets/salma.jpg)";
+  document.getElementById("giftImage1").style.backgroundImage =
+    "url(./assets/salma.jpg)";
 
-    document.getElementById("giftImage1").style.backgroundSize = "cover";
+  document.getElementById("giftImage1").style.backgroundSize = "cover";
 
-    document.getElementById("giftImage1").style.backgroundPosition = "center";
-  }
+  document.getElementById("giftImage1").style.backgroundPosition = "center";
 }
